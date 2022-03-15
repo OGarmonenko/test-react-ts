fa100c947366a62886426c7524ed6d368411472b
Olga Garmonenko <olga.garmonenko@intexsoft.by>
2022-03-15 11:08:31 +0300

    Changed styles Navbar. Added icons

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,13 +1,18 @@
 import React, {FC} from 'react';
-import {Link} from 'react-router-dom';
 import constants from '../../../constants/constants';
 import styles from './Navbar.module.css';
+import arrowLeft from './../../../assets/arrow.png';
+import {useNavigate} from 'react-router-dom';
 
 const Navbar: FC = () => {
+    let history = useNavigate();
 
     return (
         <div className={styles.wrapperNavbar}>
-            <Link className={ styles.link } to={ constants.ROUTES.MAIN_PATH } > Main </Link>
+          <div className={styles.wrapperSubNav} onClick={()=>  history(constants.ROUTES.MAIN_PATH)}>
+            <img src={arrowLeft} className={styles.arrow}></img>
+            <div className={styles.itemNavbar}>Main</div>
+          </div>
         </div>
     );
 };
